e8b4477869fd4c8d26ad4552a409efb21e2c582c
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2022-07-03 21:09:39 -0600

    v0.03
    * Added player rectange and implemented speed and direction.
    * Added scrolling to the screen.
    * Allowed player to reverse direction to scroll both sides of the screen, as needed.

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame
 from tiles import Tile
-from settings import tile_size
+from settings import tile_size, screen_width
+from player import Player
 
 class Level:
     def __init__(self, level_data, surface):
@@ -12,14 +13,39 @@ class Level:
         
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
         for row_index, row in enumerate(layout):
             for col_index, column in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
                 if column == 'X':
-                    x = col_index * tile_size
-                    y = row_index * tile_size
                     tile = Tile((x,y),tile_size)
                     self.tiles.add(tile)
-    
+                if column == 'P':
+                    player_sprite = Player((x,y))
+                    self.player.add(player_sprite)
+
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+        
+        if player_x < (screen_width * 0.25) and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > (screen_width * 0.75) and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
+
     def run(self):
-        self.tiles.update(0)
+        # Level Tiles
+        self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+        
+        # Player
+        self.player.update()
+        self.player.draw(self.display_surface)
+        self.scroll_x()
